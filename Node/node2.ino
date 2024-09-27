3280b4ac933e7061eb1bb7b0707f2d650a48a78a
RachitDuaCode <150435184+RachitDuaCode@users.noreply.github.com>
2024-09-27 22:43:57 +0530

    Initial commit

---
+++ Node/node2.ino
@@ -0,0 +1,73 @@
+#include <ESP8266WiFi.h>
+#include <ESP8266WebServer.h>
+int led=D3;
+int ss=A0;     
+
+
+const char* ssid = "RONYSLENNY 0279";
+const char* password = "10101010";
+
+ESP8266WebServer server(80);
+int variable = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(ss,INPUT);
+  pinMode(led,OUTPUT);
+
+  // Connect to Wi-Fi
+  WiFi.begin(ssid, password);
+  Serial.println("Connecting to WiFi...");
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.println("Connecting...");
+  }
+  Serial.println("Connected to WiFi");
+
+  // Initialize random seed
+  randomSeed(analogRead(A0));
+
+  // Start the web server
+  server.on("/setVariable", HTTP_GET, handleSetVariable);
+  server.on("/getVariable", HTTP_GET, handleGetVariable);
+  server.begin();
+  Serial.println("HTTP server started");
+}
+
+void loop() {
+  server.handleClient();
+}
+
+void handleSetVariable() {
+  if (server.hasArg("value")) {
+    String value = server.arg("value");
+    variable = value.toInt();
+    Serial.print("Variable set to: ");
+    Serial.println(variable);
+    server.send(200, "text/plain", "Variable set successfully");
+    Serial.print("successfully send");
+  } else {
+    server.send(400, "text/plain", "Invalid request");
+    Serial.print("Invalid request");
+  }
+}
+
+void handleGetVariable() {
+  // variable = random(501); // Generate a random number between 0 and 500
+  int variable=analogRead(ss);
+  String variableValue = String(variable);
+  server.sendHeader("Access-Control-Allow-Origin", "*");
+  server.send(200, "text/plain", variableValue);
+   if(variable>100)
+ {
+  digitalWrite(led,1);
+  delay(3000);
+ }
+ else
+ {
+  digitalWrite(led,0);
+ }
+ Serial.println(variable);
+  delay(200);
+
+}
---
+++ README.md
@@ -0,0 +1,21 @@
+
+# Sensor Zone
+
+SensorZone is an innovative project designed to manage noise levels in libraries and labs by integrating hardware like Node MCU and sound sensors with software solutions, including C++ and a user-friendly web interface. This system allows users to monitor noise levels in real time, receive alerts, and take prompt action to maintain a peaceful environment, contributing to more conducive educational spaces.
+
+## 🛠 Skills
+
+Node Mcu
+
+Html and Css
+
+C++
+## Deployment
+
+1. Connect all hardware components, including wires, a breadboard, an LED bulb, a noise sensor, and the Node MCU.
+2. Install the Node MCU compiler and connect the Node MCU to your laptop using a USB cable.
+3. Integrate the website with the Node MCU, check connectivity, and ensure the bulb lights up as a noise indication.
+
+
+## Screenshots
+
